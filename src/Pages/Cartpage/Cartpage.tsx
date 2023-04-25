e6a6a6de710b428cc3166cefca294d8dfc371696
gkumar-pandey <gautamshekhar078@gmail.com>
2023-04-25 15:45:16 +0530

    update

--- src/Pages/Cartpage/Cartpage.tsx
+++ src/Pages/Cartpage/Cartpage.tsx
@@ -4,14 +4,9 @@ import FoodCard from "../../Components/FoodCard/FoodCard";
 import GridContainer from "../../Components/GridContainer/GridContainer";
 import Heading from "../../Components/Heading/Heading";
 import './cart.css'
-import { useState } from "react";
 
 const Cartpage = () => {
-    const { cart, totalDeliveryTime, totalPrice, applyCouponHandler, setIsApplyCoupon } = useCart();
-
-
-
-
+    const { cart, totalDeliveryTime, totalPrice, applyCouponHandler } = useCart();
 
     return (
         <div>
@@ -24,7 +19,6 @@ const Cartpage = () => {
                 </div>
                 <GridContainer>
                     {cart.map((menu: any) => (
-
                         <FoodCard key={menu.id} {...menu} />
                     ))}
                 </GridContainer>
